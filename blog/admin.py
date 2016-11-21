3499cad5ee29d0200d23661e98243d160bc76943
Cristhian <cristhian-9292@hotmail.com>
2016-11-21 11:58:50 -0600

    Registro de Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Editor
+from .models import Editor, Autor, Libro
 
 admin.site.register(Editor)
+admin.site.register(Autor)
+admin.site.register(Libro)
